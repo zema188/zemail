2d2069b51d2a4350419ec26e1859a58d89630ba0
Artem Zimin <sasha.lopa188@gmail.com>
2023-09-19 16:12:38 +0300

    initial commit

---
+++ nuxt.config.ts
@@ -0,0 +1,17 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  modules: ['@nuxtjs/supabase'],
+  supabase: {
+    "redirect": false,
+  },
+  css: ['vuetify/lib/styles/main.sass'],
+  build: {
+    transpile: ['vuetify'],
+  },
+  vite: {
+    define: {
+      'process.env.DEBUG': false,
+    },
+  },
+})
